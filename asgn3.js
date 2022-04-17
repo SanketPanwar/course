1f9dd890c6cea0364bff6cb59fa7703c0423eae1
sanket <sanketdpr@gmail.com>
2022-04-17 21:41:11 +0530

    assignment 3

---
+++ asgn3.js
@@ -0,0 +1,13 @@
+// console.log(document.title);
+// console.log(document.domain);
+// console.log(document.all);
+ const headerTitle=document.getElementById('header-title');
+//headerTitle.textContent='Hello';
+headerTitle.style.backgroundColor='#000000';
+const additems=document.getElementsByClassName('title')
+console.log(additems);
+additems[0].style.fontWeight='bold';
+additems[0].style.color= 'green';
+
+
+

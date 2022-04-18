d3d75c0c5ed76662db180ac1b556302c9228e62f
sanket <sanketdpr@gmail.com>
2022-04-18 09:37:15 +0530

    assignment 4 done

--- assign4/a4.js
+++ assign4/a4.js
@@ -1,1 +1,6 @@
-console.log('hello')
+//getElementsByClassName
+const items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(let i=0;i<items.length;i++)
+items[i].style.fontWeight='bold';

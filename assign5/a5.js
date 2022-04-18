4a383d478b88cd402b5c6d13f127f9d905c1290b
sanket <sanketdpr@gmail.com>
2022-04-18 10:23:26 +0530

    assignment 5 done

---
+++ assign5/a5.js
@@ -0,0 +1,14 @@
+
+const items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(let i=0;i<items.length;i++)
+items[i].style.fontWeight='bold';
+
+
+
+
+const li=document.getElementsByTagName('li');
+li[1].textContent='Hello 2';
+for(let i=0;i<li.length;i++)
+li[i].style.backgroundColor= "#f4f4f4"
